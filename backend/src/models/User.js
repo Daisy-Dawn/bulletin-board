31b1c92ef250162bc2833b6b0a35cb7890f68982
Daisy-Dawn <agboella20@gmail.com>
2024-12-09 03:16:08 +0100

    bug fixes

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -2,10 +2,16 @@ const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema(
     {
-        username: { type: String, required: true, unique: true },
+        googleId: { type: String, unique: true },
         displayName: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
-        password: { type: String, required: true, select: false },
+        username: { type: String, unique: true, required: true },
+        email: { type: String, unique: true, required: true },
+        password: {
+            type: String,
+            required: function () {
+                return !this.googleId
+            },
+        }, // Required only for non-Google users
     },
     { timestamps: true }
 )
